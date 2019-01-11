62a846dc66cec89979865f1e21c7b548233edb67
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-11 15:11:37 +0000

    Set up basic game functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+
 import { configureStore } from './state/store';
 import App from './components/App';
 
-// At a later point, we can pull the state stored in local storage (or another source)
-// and use it to create the store from a previous state
-const initialState = {};
+import { gameOperations } from './state/ducks/game';
 
-const store = configureStore(initialState);
+// At a later point, we can pull the state stored in local storage (or another source)
+// and use it to create the store from a previous state.
+
+const initialState = null;
+
+const store = configureStore(initialState || {});
+
+if (!initialState) {
+  // since we don't have any persisted state, we should start a new game when the game loads
+
+  // our new game operation returns an action object that we can use in the 
+  // redux store to dispatch
+  const newGame = gameOperations.newGame();
+
+  store.dispatch(newGame);
+}
 
 render(
   <Provider store={store}>
--- src/state/ducks/game/test.js
+++ src/state/ducks/game/test.js
@@ -1,5 +1,8 @@
 import reducer from './reducers';
 import * as actions from './actions';
+import * as operations from './operations';
+
+// TODO: consider breaking out into specific test files
 
 describe('Game Duck', () => {
   const emptyBoard = [
@@ -177,6 +180,83 @@ describe('Game Duck', () => {
       const result2 = reducer(state, action);
 
       expect(result2).toEqual(expectedState2);
+    });
+  });
+
+  describe('Operations', () => {
+    // note that we are only test our complex operations since
+    // the reducers already test the simple actions
+
+    const { checkWinner, playTurn } = operations;
+
+    it('should dispatch a winner', () => {
+      const dispatch = jest.fn();
+      const board = player1WinBoard;
+      const player = 1;
+
+      const winnerAction = actions.winner(1);
+      const gameoverAction = actions.gameover();
+
+      checkWinner(board, player)(dispatch);
+
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0][0]).toEqual(winnerAction);
+      expect(dispatch.mock.calls[1][0]).toEqual(gameoverAction);
+    });
+
+    it('should dispatch a draw', () => {
+      const dispatch = jest.fn();
+      const board = drawBoard;
+      const player = 1;
+
+      const winnerAction = actions.winner(0);
+      const gameoverAction = actions.gameover();
+
+      checkWinner(board, player)(dispatch);
+
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0][0]).toEqual(winnerAction);
+      expect(dispatch.mock.calls[1][0]).toEqual(gameoverAction);
+    });
+
+    it('should not dispatch if game is in progress', () => {
+      const dispatch = jest.fn();
+      const board = emptyBoard;
+      const player = 1;
+
+      checkWinner(board, player)(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
+    });
+
+    it('should play a turn', () => {
+      const dispatch = jest.fn();
+
+      let player = 1;
+      let row = 0;
+      let col = 0;
+
+      const move1 = actions.movePlayer(player, row, col);
+      const switch1 = actions.switchPlayer(2); // the operation will switch
+
+      playTurn(player, row, col)(dispatch);
+
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0][0]).toEqual(move1);
+      expect(dispatch.mock.calls[1][0]).toEqual(switch1);
+
+      player = 2;
+      row = 1;
+      col = 1;
+
+      const move2 = actions.movePlayer(player, row, col);
+      const switch2 = actions.switchPlayer(1); // the operation will switch
+
+      playTurn(player, row, col)(dispatch);
+
+      expect(dispatch).toHaveBeenCalledTimes(4);
+      expect(dispatch.mock.calls[2][0]).toEqual(move2);
+      expect(dispatch.mock.calls[3][0]).toEqual(switch2);
     });
   });
 });

3b6c4d040b5cac54817094265926b0543836577e
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-11 16:37:31 +0000

    Add header

--- src/components/GameoverDialog.js
+++ src/components/GameoverDialog.js
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
+import withMobileDialog from "@material-ui/core/withMobileDialog";
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-
-import Dialog, {
-  withMobileDialog,
-} from 'material-ui/Dialog';
 
 const GameoverDialog = ({ fullScreen, open, isDraw, player, onClick, onClose }) => {
   const title = isDraw ? 'Draw!' : `Player - ${player} wins!`;
